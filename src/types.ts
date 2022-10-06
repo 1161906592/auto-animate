79110103bc20c4c00e64493fde92c2f9d6539f06
liuyang.0826 <1161906592@qq.com>
2022-10-06 22:20:24 +0800

    fix: change undefined to void

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
 export interface Options {
-  add?(el: Element): KeyframeEffect | undefined
-  remove?(el: Element, oldRect: DOMRect): KeyframeEffect | undefined
-  update?(el: Element, oldRect: DOMRect): KeyframeEffect | undefined
+  add?(el: Element): KeyframeEffect | void
+  remove?(el: Element, oldRect: DOMRect): KeyframeEffect | void
+  update?(el: Element, oldRect: DOMRect): KeyframeEffect | void
   duration?: number
   easing?: 'linear' | 'ease-in' | 'ease-out' | 'ease-in-out' | string
 }
