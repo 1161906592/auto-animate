9b2009ea306025970db5a37dec2a51266253287d
liuyang.0826 <1161906592@qq.com>
2022-10-28 11:12:51 +0800

    feat(EaseText): support easeOnUpdate

--- src/vue/EaseText.ts
+++ src/vue/EaseText.ts
@@ -1,4 +1,5 @@
 import { defineComponent, h } from 'vue'
+import { duration } from '@liuyang0826/auto-animate'
 import type { PropType } from 'vue'
 import { useAutoAnimate } from './useAutoAnimate'
 
@@ -6,23 +7,29 @@ export const EaseText = /* @__PURE__ */ defineComponent({
   props: {
     text: [String, Number] as PropType<string | number | null>,
     tag: { type: String as PropType<string>, default: 'span' },
+    easeOnUpdate: Boolean as PropType<boolean>,
   },
   setup(props) {
     const { rootRef } = useAutoAnimate({
       add: (el) =>
         new KeyframeEffect(el, [{ opacity: 0 }, { opacity: 1 }], {
-          duration: 300,
+          duration,
           easing: 'ease-in',
         }),
       remove: (el) =>
         new KeyframeEffect(el, [{ opacity: 1 }, { opacity: 0 }], {
-          duration: 300,
+          duration,
           easing: 'ease-out',
         }),
     })
 
     const style = 'display: inline-block;vertical-align: middle;'
 
-    return () => h(props.tag, { ref: rootRef, style }, props.text ? h('span', { style }, props.text) : undefined)
+    return () =>
+      h(
+        props.tag,
+        { ref: rootRef, style },
+        props.text ? h('span', { style, key: props.easeOnUpdate ? props.text : undefined }, props.text) : undefined
+      )
   },
 })
